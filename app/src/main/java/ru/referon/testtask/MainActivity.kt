ff077c5d3bb0b6742993e2957ddb36e605b25fda
Arseny <senika-2010@mail.ru>
2022-03-29 09:02:43 +1000

    updated MainActivity

--- app/src/main/java/ru/referon/testtask/MainActivity.kt
+++ app/src/main/java/ru/referon/testtask/MainActivity.kt
@@ -1,11 +1,35 @@
 package ru.referon.testtask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import ru.referon.testtask.databinding.ActivityMainBinding
+import ru.referon.testtask.viewmodel.ViewModel
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: ViewModel by viewModels()
+        viewModel.loadFoods()
+        binding.b1.isSelected = true
+
+        binding.b1.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
+
+        val adapter = Adapter()
+        binding.list.adapter = adapter
+
+        viewModel.data.observe(this) { model ->
+            binding.loading.isVisible = model.loading
+            adapter.submitList(model.foods?.results)
+        }
+
     }
 }
